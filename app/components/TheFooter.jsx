31a78910172640c90b210187a965213ee497c05c
IamErfanImmortal <erf.developer88@gmail.com>
2024-02-16 23:34:46 +0100

    change images

--- app/components/TheFooter.jsx
+++ app/components/TheFooter.jsx
@@ -2,7 +2,9 @@ export default function TheFooter() {
     return (
         <footer>
             <ul className="footer__menu">
-                <h3><a href="/" rel="home" className="header__logo">Erfan</a></h3>
+                <h3><a href="/" rel="home" className="footer__logo">
+                    <img src={require("../images/footerlogo.png")} alt="" />
+                    </a></h3>
                 <li><a className="header__menu-link" href="#x.com">Portfolio</a></li>
                 <li><a className="header__menu-link" href="#linkedin">About</a></li>
                 <li><a className="header__menu-link" href="#facebook">Contact</a></li>
