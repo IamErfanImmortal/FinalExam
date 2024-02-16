a4bb8ea9dc2061c457c74e21b2066698e5c07437
IamErfanImmortal <erf.developer88@gmail.com>
2024-02-16 13:58:50 +0100

    add header

--- app/root.jsx
+++ app/root.jsx
@@ -5,8 +5,27 @@ import {
     Outlet,
     Scripts,
   } from "@remix-run/react";
-  
+  import styles from "./styles/app.css";
+  import TheHeader from "./components/TheHeader/TheHeader";  
+  import TheHeaderStyles from "./components/TheHeader/TheHeader.css";
+  export const meta = () => [
+    {
+      name: "viewport",
+      content: "width=device-width, initial-scale=1"
+    },
+  ];
+  export const links = () => [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+    {
+      rel: "stylesheet",
+      href: TheHeaderStyles,
+    },
+  ];
   export default function App() {
+    const menuItems = ['Portfolio', 'About',"Contact"];
     return (
       <html>
         <head>
@@ -18,9 +37,13 @@ import {
           <Links />
         </head>
         <body>
-          <h1>Hello world!s</h1>
+            <TheHeader title="Erfan" menuItems={menuItems}/>
+          <h1>Hello world!</h1>
           <Outlet />
-  
+          <section className="project-card">
+            {/* <img src={pic1} alt="pic" /> */}
+          </section>
+            
           <Scripts />
           <LiveReload />
         </body>
